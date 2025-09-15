6fe555ee7246fe464bdd195252d72f9add3f4761
Vishnu <vishnu.rachamallu@dluxtech.com>
2025-09-15 15:11:23 +0530

    hero js update to log html

---
+++ blocks/hero/hero.js
@@ -0,0 +1,9 @@
+export default function decorate(block) {
+/* This JavaScript makes minor adjustments to the block's DOM */
+
+  /* Common treatments for all options */
+  console.log("=====");
+  console.log("hero block");
+  console.log(block);
+  console.log("end =====");
+}
