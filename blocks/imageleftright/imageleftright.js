dc7274c7ee19884d7e0d0999f9f9ff589eeb3772
sasi.chelluri <sasi.kanth@invocare.com.au>
2025-10-28 19:21:29 +0530

    imageleftright code

--- blocks/imageleftright/imageleftright.js
+++ blocks/imageleftright/imageleftright.js
@@ -1,18 +1,20 @@
-// /blocks/sidebyside/sidebyside.js
-
-function getOptions(block) {
-  return [...block.classList].filter((c) => !['block', 'sidebyside'].includes(c));
-}
-
 export default function decorate(block) {
-  const swap = getOptions(block).includes('swap');
-  const imageDiv = block.querySelector('.sidebyside-image');
-  const contentDiv = block.querySelector('.sidebyside-content');
-
   block.classList.add('sidebyside-layout');
+  const swap = block.classList.contains('swap');
 
+  // Find and mark children
+  const imageDiv = block.querySelector('img')?.closest('div');
+  const contentDiv = block.querySelector('h2, h3, h4, h5, h6')?.closest('div');
+
+  if (imageDiv) imageDiv.classList.add('sidebyside-image');
+  if (contentDiv) contentDiv.classList.add('sidebyside-content');
+
+  // Force correct order
   if (swap && imageDiv && contentDiv) {
-    // Swap positions if "swap" class present
-    block.insertBefore(contentDiv, imageDiv);
+    block.prepend(contentDiv);
+    block.append(imageDiv);
+  } else if (imageDiv && contentDiv) {
+    block.prepend(imageDiv);
+    block.append(contentDiv);
   }
 }
